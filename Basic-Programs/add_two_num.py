68db921420f7c1c7488bb72991db26b5586bc67f
anshu <anshu.pathak@logicielsolutions.co.in>
2020-11-21 20:22:21 +0530

    add operation

---
+++ Basic-Programs/add_two_num.py
@@ -0,0 +1,14 @@
+x = 10
+y = 20
+z = x + y
+print(z)
+
+# Performe the add operation on the bases of the user enter values.
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+print(sum)
